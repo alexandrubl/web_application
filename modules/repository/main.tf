3a01b3fa33d01064b1da6dabcafe5cdb687ea7e3
alexandrublg <103655064+alexandrublg@users.noreply.github.com>
2022-05-24 16:36:18 +0300

    Update main.tf

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -1,8 +1,3 @@
-resource "aws_ecrpublic_repository" "dev_ecr_repo" {
+resource "aws_ecrpublic_repository" "ecr_repo" {
   repository_name                 = "${var.ecr_name}-repo"
-#  image_tag_mutability = "MUTABLE"
-
-#  image_scanning_configuration {
-#    scan_on_push = true
-#  }
 }
